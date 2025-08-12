b094032d6d0631d19b5b32b0f2e02344ce5e2ebe
Diego <bencherifdiego@gmail.com>
2025-08-12 21:47:19 +0200

    tijdlijn in detail gezet

--- frontend/src/app/admin/detail/detail.ts
+++ frontend/src/app/admin/detail/detail.ts
@@ -1,15 +1,25 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ActivatedRoute } from '@angular/router';
+import { TijdlijnElementen } from '../tijdlijn-element/tijdlijn-element';
+import { TijdlijnElement } from '../tijdlijn-element.interface';
+
+const tijdlijnElementen: TijdlijnElement[] = [
+  {status: "Nieuw", note: "test", date: new Date(2020, 1, 1)},
+  {status: "Gevontrolleerd", note: "test", date: new Date(2020, 1, 2)},
+  {status: "Ingepland", note: "test", date: new Date(2020, 1, 4)},
+  {status: "Opgehaald", note: "test", date: new Date(2020, 1, 5)},
+]
 
 @Component({
   selector: 'app-detail',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, TijdlijnElementen],
   templateUrl: './detail.html',
   styleUrl: './detail.scss'
 })
 export class DetailComponent {
+  TEST = tijdlijnElementen;
   id: number | null = null;
 
   constructor(private route: ActivatedRoute) {
--- frontend/src/app/admin/tijdlijn-element/tijdlijn-element.ts
+++ frontend/src/app/admin/tijdlijn-element/tijdlijn-element.ts
@@ -1,9 +1,10 @@
 import { Component, input } from '@angular/core';
 import { TijdlijnElement } from '../tijdlijn-element.interface';
+import { MatCard } from '@angular/material/card';
 
 @Component({
   selector: 'app-tijdlijn-element',
-  imports: [],
+  imports: [MatCard],
   templateUrl: './tijdlijn-element.html',
   styleUrl: './tijdlijn-element.scss'
 })
