bb3b0bc3455e3a4b963e532259fe43090fda4a3f
Joël <joelsefanja@gmail.com>
2025-08-12 11:54:31 +0200

    Routes opzetten voor app gebruikers en gemeente gebruikers zodat beide op een apart scherm bereikbaar zijn.

---
+++ frontend/src/app/app-users/app-users.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-app-users',
+  imports: [],
+  templateUrl: './app-users.html',
+  styleUrl: './app-users.scss'
+})
+export class AppUsersComponent {
+
+}
--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { AppUsersComponent } from './app-users/app-users';
+import { AdminComponent } from './admin/admin';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: AppUsersComponent },
+  { path: 'admin', component: AdminComponent },
+  { path: '**', redirectTo: '' }
+];
