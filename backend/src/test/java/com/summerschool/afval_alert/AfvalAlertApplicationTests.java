9c6e847f133904e7011e8c1b399e97423f83c916
Thijs Zijlstra <65619302+thijszijlstra@users.noreply.github.com>
2025-08-13 14:48:41 +0200

    Toevoegen gegenereerde spring initializer app

---
+++ backend/src/test/java/com/summerschool/afval_alert/AfvalAlertApplicationTests.java
@@ -0,0 +1,13 @@
+package com.summerschool.afval_alert;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AfvalAlertApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
