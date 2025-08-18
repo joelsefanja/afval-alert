e41d75743c19e912d0dcd1908da17e381aea0301
Diego <bencherifdiego@gmail.com>
2025-08-18 15:38:47 +0200

    added endpoint and function to update status of melding

---
+++ backend/src/main/java/com/summerschool/afval_alert/model/dto/PutStatusMeldingDTO.java
@@ -0,0 +1,11 @@
+package com.summerschool.afval_alert.model.dto;
+
+import com.summerschool.afval_alert.model.enums.Status;
+
+public class PutStatusMeldingDTO {
+    private Status status;
+
+    public Status getStatus() {
+        return status;
+    }
+}
