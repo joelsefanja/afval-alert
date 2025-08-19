75247722caaa407c70d35b662b5344f7a8c11be5
Diego <bencherifdiego@gmail.com>
2025-08-19 14:04:14 +0200

    Update ShowMeldingDTO.java

--- backend/src/main/java/com/summerschool/afval_alert/model/dto/ShowMeldingDTO.java
+++ backend/src/main/java/com/summerschool/afval_alert/model/dto/ShowMeldingDTO.java
@@ -18,7 +18,6 @@ public class ShowMeldingDTO {
     private List<NotitieDTO> notities;
     private LocalDateTime createdAt;
 
-
     public void setId(Long id) {
         this.id = id;
     }
@@ -46,7 +45,6 @@ public class ShowMeldingDTO {
     public void setCreatedAt(LocalDateTime createdAt) {
         this.createdAt = createdAt;
     }
-
 
     public Long getId() {
         return id;
