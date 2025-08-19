5cf810d1a9e433be17477b4f493b7affec316222
Diego <bencherifdiego@gmail.com>
2025-08-19 13:38:09 +0200

    cleanup dto getters and setters

--- backend/src/main/java/com/summerschool/afval_alert/model/dto/ShowMeldingDTO.java
+++ backend/src/main/java/com/summerschool/afval_alert/model/dto/ShowMeldingDTO.java
@@ -18,75 +18,61 @@ public class ShowMeldingDTO {
     private List<NotitieDTO> notities;
     private LocalDateTime createdAt;
 
-    public Long getId() {
-        return id;
-    }
 
     public void setId(Long id) {
         this.id = id;
     }
-
-    public double getLat() {
-        return lat;
-    }
-
     public void setLat(double lat) {
         this.lat = lat;
     }
-
-    public double getLon() {
-        return lon;
-    }
-
     public void setLon(double lon) {
         this.lon = lon;
     }
-
-    public String getComment() {
-        return comment;
-    }
-
     public void setComment(String comment) {
         this.comment = comment;
     }
-
-    public String getImageUrl() {
-        return imageUrl;
-    }
-
     public void setImageUrl(String imageUrl) {
         this.imageUrl = imageUrl;
     }
-
-    public Status getStatus() {
-        return status;
-    }
-
     public void setStatus(Status status) {
         this.status = status;
     }
-
-    public TrashType getTrashType() {
-        return trashType;
-    }
-
     public void setTrashType(TrashType trashType) {
         this.trashType = trashType;
     }
-
-    public List<NotitieDTO> getNotities() {
-        return notities;
-    }
-
     public void setNotitie(List<NotitieDTO> notities) {
         this.notities = notities;
     }
-
-    public LocalDateTime getCreatedAt() {
-        return createdAt;
-    }
-
     public void setCreatedAt(LocalDateTime createdAt) {
         this.createdAt = createdAt;
+    }
+
+
+    public Long getId() {
+        return id;
+    }
+    public double getLat() {
+        return lat;
+    }
+    public double getLon() {
+        return lon;
+    }
+    public String getComment() {
+        return comment;
+    }
+    public String getImageUrl() {
+        return imageUrl;
+    }
+    public Status getStatus() {
+        return status;
+    }
+    public TrashType getTrashType() {
+        return trashType;
+    }
+    public List<NotitieDTO> getNotities() {
+        return notities;
+    }
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
     }
 }
